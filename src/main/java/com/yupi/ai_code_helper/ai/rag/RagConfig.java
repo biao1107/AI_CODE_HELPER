ecc1ecf70bc663728e2a1b320365b2191a9436f6
itcast <hello@itcast.cn>
2026-05-26 11:33:41 +0800

    Rag检索增强生成

--- src/main/java/com/yupi/ai_code_helper/ai/rag/RagConfig.java
+++ src/main/java/com/yupi/ai_code_helper/ai/rag/RagConfig.java
@@ -3,22 +3,16 @@ package com.yupi.ai_code_helper.ai.rag;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.splitter.DocumentByParagraphSplitter;
-import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.data.segment.TextSegmentTransformer;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
-import dev.langchain4j.store.embedding.IngestionResult;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
 import java.util.List;
 
 @Configuration
