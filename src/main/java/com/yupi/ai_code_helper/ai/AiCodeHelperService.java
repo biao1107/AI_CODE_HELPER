f9028752ff469ac2346ba9cc14b850b5e8e49f10
itcast <hello@itcast.cn>
2026-05-26 17:48:49 +0800

    服务化sse流式化输出

--- src/main/java/com/yupi/ai_code_helper/ai/AiCodeHelperService.java
+++ src/main/java/com/yupi/ai_code_helper/ai/AiCodeHelperService.java
@@ -1,9 +1,12 @@
 package com.yupi.ai_code_helper.ai;
 
 import com.yupi.ai_code_helper.ai.guardrail.SafeInputGuardrail;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.Result;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.guardrail.InputGuardrails;
+import reactor.core.publisher.Flux;
 
 
 import java.util.List;
@@ -13,14 +16,17 @@ public interface AiCodeHelperService {
 
     @SystemMessage(fromResource = "system-prompt.txt")
     String chat(String userMessage);
-
+    //报告
     @SystemMessage(fromResource = "system-prompt.txt")
     Report chatForReport(String userMessage);
 
     record Report(String name, List<String> suggestionList){};
 
-
+    // rag
     @SystemMessage(fromResource = "system-prompt.txt")
     Result<String> chatWithRag(String userMessage);
 
+    //流式输出
+    @SystemMessage(fromResource = "system-prompt.txt")
+    Flux<String> chatStream(@MemoryId  int memoryId,@UserMessage String message);
 }
--- src/main/java/com/yupi/ai_code_helper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/yupi/ai_code_helper/ai/AiCodeHelperServiceFactory.java
@@ -4,6 +4,7 @@ import com.yupi.ai_code_helper.ai.tools.InterviewQuestionTool;
 import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
@@ -20,6 +21,8 @@ public class AiCodeHelperServiceFactory {
     @Resource
     private McpToolProvider   mcpToolProvider;
 
+    @Resource
+    private StreamingChatModel qwenStreamingChatModel;
     @Bean
     public AiCodeHelperService aiCodeHelperService(){
 
@@ -28,7 +31,9 @@ public class AiCodeHelperServiceFactory {
         //构建Ai Service
         AiCodeHelperService aiCodeHelperService = AiServices.builder(AiCodeHelperService.class)
                 .chatModel(MyQwenChatModel)
-                .chatMemory(chatMemory)//会话记忆
+                .streamingChatModel(qwenStreamingChatModel)//流式输出
+                .chatMemoryProvider(memoryId ->MessageWindowChatMemory.withMaxMessages(10))//每个会话独立存储
+                /*.chatMemory(chatMemory)//会话记忆*/
                 .contentRetriever(contentRetriever)//Rag 检索增强生成
                 .tools(new InterviewQuestionTool())//tool工具调用
                 .toolProvider(mcpToolProvider)//mcp工具调用
